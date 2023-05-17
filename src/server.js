c490db7ae5b214ab84855cd06086f532af881ab6
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-16 21:30:35 -0500

    person query working

--- src/server.js
+++ src/server.js
@@ -11,7 +11,21 @@ app.use(express.json());
 
 app.get('/', (req, res, next    ) => {
   res.status(200).send('proof of life');
+
 });
+
+app.get('/person', (req, res, next) => {
+  let name = req.query.name;
+  if (!name) {
+    res.status(500).send('Name parameter is missing');
+  }
+  else {
+    res.status(200).json({name});
+
+  }
+},
+);
+
 
 app.get('/success', (req, res, next) => {
   res.status(200).send('Success!!');
@@ -19,12 +33,12 @@ app.get('/success', (req, res, next) => {
 
 app.get('/bad', (req, res, next) => {
   next('you messsed up');
-}
+},
 );
 
 app.use('*', (req, res, next) => {
   res.status(404).send('not found');
-}
+},
 );
 
 const start = (port) => app.listen(port, () => console.log(`server up on port ${port}`));
