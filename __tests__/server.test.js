26a3991ee05fd6b6725bbf43b4c2c502019e4d0e
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-17 09:56:11 -0500

    added 500 test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -13,29 +13,35 @@ describe('Server', () => {
       expect(response.text).toBeTruthy;
       expect(response.text).toBe('proof of life');
     });
+});
 
 
-  test('handles success route', async () => {
-    const response = await mockRequest.get('/success');
+test('handles success route', async () => {
+  const response = await mockRequest.get('/success');
 
-    expect(response.status).toEqual(200);
-    expect(response.text).toEqual('Success!!');
+  expect(response.status).toEqual(200);
+  expect(response.text).toEqual('Success!!');
 
-  });
+});
 
-  test('handles bad requests', async () => {
-    const response = await mockRequest.get('/bad');
-    expect(response.status).toBe(500);
-  }   );
+test('handles bad requests', async () => {
+  const response = await mockRequest.get('/bad');
+  console.log(response.body);
+  expect(response.status).toBe(500);
+  expect(response.body.route).toBe('/bad');
+}   );
 
-  test('handles not found', async () => {
-    const response = await mockRequest.get('/notfound');
-    expect(response.status).toBe(404);
-  }   );
+
+
+
+test('handles not found', async () => {
+  const response = await mockRequest.get('/notfound');
+  expect(response.status).toBe(404);
+}   );
 //TDD goal: write a test that proves that the /person route works with a name query string parameter
 test('handles person route', async () => {
-    const response = await mockRequest.get('/person?name=John');
-    expect(response.status).toBe(200);
-    expect(response.body.name).toEqual('John');
+  const response = await mockRequest.get('/person?name=John');
+  expect(response.status).toBe(200);
+  expect(response.body.name).toEqual('John');
 
 });
