7309028fe8ce9a6a8be7ec7e477ba06fc3e0cdad
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-16 20:46:07 -0500

    updated server test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,8 +9,18 @@ describe('Server', () => {
     async ()=> {
       const response = await mockRequest.get('/');
       expect(response.status).toBe(200);
+      expect(response.text).toBeTruthy;
       expect(response.text).toBe('proof of life');
     });
+
+
+  test('handles success route', async () => {
+    const response = await mockRequest.get('/success');
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual('Success!!');
+
+  });
 
   test('handles bad requests', async () => {
     const response = await mockRequest.get('/bad');
