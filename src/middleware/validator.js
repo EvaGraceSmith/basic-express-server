a5d41e09a37361cc0ff7d1c76f28b68c8bd5eff7
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-16 23:28:29 -0500

    added logger middleware

---
+++ src/middleware/validator.js
@@ -0,0 +1,2 @@
+//Checks the query string for a name property
+//Sends the request through when valid, forces an error when not
--- src/server.js
+++ src/server.js
@@ -2,18 +2,21 @@
 
 const express = require('express');
 const cors = require('cors');
+const logger = require('./middleware/logger.js');
 
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.get('/', (req, res, next    ) => {
   res.status(200).send('proof of life');
 
 });
 
+//query string
 app.get('/person', (req, res, next) => {
   let name = req.query.name;
   if (!name) {
@@ -21,10 +24,18 @@ app.get('/person', (req, res, next) => {
   }
   else {
     res.status(200).json({name});
-
+    console.log('req.query', req.query.name);
   }
 },
 );
+
+//path (or URL) parameters
+app.get('/helloPath/someName', (req, res, next) => {
+  console.log('params' ,req.params);
+  res.status(200).send('Hello from the path');
+},
+);
+
 
 
 app.get('/success', (req, res, next) => {
